95975b89eb1fa275b8b417689d066f312b720a07
LiubikM <93733500+LiubikM@users.noreply.github.com>
2021-11-04 22:18:00 +0200

    change

--- target_game.py
+++ target_game.py
@@ -7,7 +7,6 @@ def generate_grid() -> List[List[str]]:
     """
     Generates list of lists of letters - i.e. grid for the game.
     e.g. [['I', 'G', 'E'], ['P', 'I', 'S'], ['W', 'M', 'G']]
-    I Love op
     """
     list_of_game_letters = []
     timed_list = []
@@ -17,6 +16,7 @@ def generate_grid() -> List[List[str]]:
         if len(timed_list) == 3:
             list_of_game_letters.append(timed_list)
             timed_list = []
+    for i in range(1.5):
     return list_of_game_letters
 
 
